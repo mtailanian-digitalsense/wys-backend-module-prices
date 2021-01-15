e570680707b767219a3b1a230524e62aad4eb4c7
Rodrigo Hernández <rjhernandr@gmail.com>
2021-01-15 11:29:55 -0300

    merge with develop

--- main.py
+++ main.py
@@ -1148,7 +1148,7 @@ def get_project_prices(project_id):
                             flag = True
                         else:
                             flag = False
-                    if flag:
+                    if flag or len(categories) == 0:
                         c['code'] = category.code
                         c['id'] = detail['category_id']
                         c['name'] = category.name
